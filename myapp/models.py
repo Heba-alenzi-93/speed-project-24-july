4133c68dde3cbc95035abc94fab7e8b5122929be
apple <apple@apples-MacBook-Pro.local>
2022-07-24 16:49:42 +0300

    finish

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+ 
+class studentInfo(models.Model):
+
+    status_choies ={
+        ("Python","Python"),
+        ("Django","Django"),
+         ("Flutter","Flutter")
+    }  
+    
+    studentName = models.CharField(max_length=250)
+    studentID = models.CharField(max_length=250)
+    subject = models.CharField(choices= status_choies,max_length=250,default="----")
+
+
+    def __str__(self) :
+        return self.studentName
+
+     
+
+
+
+
+ 
